7e4b8c5ff92e797394132924aa481166c987f1f8
jimmynguyen <jimmynguyen@protonmail.com>
2017-10-13 02:06:14 -0400

    initialized easy challenge 1 java solution

---
+++ java/c001e.java
@@ -0,0 +1,24 @@
+/*
+ * Solution to /r/dailyprogrammer Challenge #1 Easy
+ * Jimmy Nguyen
+ * 
+ * Prompt:
+ *     Create a program that will ask the user's name, age, and reddit username.
+ * Have it tell them the information back, in the format:
+ * =============================================================================
+ * your name is (blank), you are (blank) years old, and your username is (blank)
+ * =============================================================================
+ * 
+ * Bonus:
+ *     For extra credit, have the program log this information in a file to be
+ * accessed later.
+ * 
+ * Link to challenge:
+ *     https://www.reddit.com/r/dailyprogrammer/comments/pih8x/easy_challenge_1/
+ */
+
+public final class c001e {
+	public static void main(String[] args) {
+		
+	}
+}
