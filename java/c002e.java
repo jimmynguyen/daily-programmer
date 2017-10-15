580fdd1d45a0ae00dcd357e5d23092fde60da89e
jimmynguyen <jimmynguyen@protonmail.com>
2017-10-15 15:42:03 -0400

    added easy challenge 2 java solution

---
+++ java/c002e.java
@@ -0,0 +1,80 @@
+/*
+ * Solution to /r/dailyprogrammer Challenge #1 Hard
+ * Jimmy Nguyen
+ *
+ * Prompt:
+ *     Create a program that will guess numbers between 1 and 100, and respond
+ * appropriately based on whether the users say that number is too high or too
+ * low
+ *
+ * Link to challenge:
+ *     https://www.reddit.com/r/dailyprogrammer/comments/pii6j/difficult_challenge_1/
+ */
+import java.util.Arrays;
+import java.util.Scanner;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+public final class c002e {
+	private static Scanner scanner;
+
+	private static void print_menu() {
+		System.out.printf("\n==================================================\n");
+		System.out.printf("\n\t1. F = M * A");
+		System.out.printf("\n\t2. M = F / A");
+		System.out.printf("\n\t3. A = F / M");
+		System.out.printf("\n\tq. quit");
+		System.out.printf("\n\n\t-------------------------\n");
+		System.out.printf("\n\t>> ");
+	}
+
+	private static String input(String prompt) {
+		System.out.print(prompt);
+		return scanner.nextLine();
+	}
+
+	private static void prompt_and_compute(String desired_variable) {
+		String[] variables = new String[] {"F", "M", "A"};
+		for (String variable : variables) {
+			if (variable.equals(desired_variable)) {
+				variables = ArrayUtils.removeElement(variables, variable);
+				break;
+			}
+		}
+		System.out.println();
+		double input1 = Double.valueOf(input(String.format("\t%s = ", variables[0])));
+		double input2 = Double.valueOf(input(String.format("\t%s = ", variables[1])));
+		double output;
+		if (Arrays.asList("M", "A").contains(desired_variable)) {
+			output = input1 / input2;
+		} else {
+			output = input1 * input2;
+		}
+		System.out.printf("\n\t%s = %.2f", desired_variable, output);
+		System.out.printf("\n\n\tPress ENTER to go back to the menu...");
+		input("");
+	}
+
+	private static boolean select_formula(String command) {
+		boolean is_terminate = false;
+		if ("1".equals(command)) {
+			prompt_and_compute("F");
+		} else if ("2".equals(command)) {
+			prompt_and_compute("M");
+		} else if ("3".equals(command)) {
+			prompt_and_compute("A");
+		} else if ("q".equals(command)) {
+			is_terminate = true;
+		}
+		return is_terminate;
+	}
+
+	public static void main(String[] args) {
+		scanner = new Scanner(System.in);
+		boolean is_terminate = false;
+		while (!is_terminate) {
+			print_menu();
+			is_terminate = select_formula(input(""));
+		}
+	}
+}
