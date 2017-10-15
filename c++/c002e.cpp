cd183d67e89e2f3d1356b9af3ca632d994b0d982
jimmynguyen <jimmynguyen@protonmail.com>
2017-10-15 16:15:59 -0400

    added easy challenge 2 c++ solution

---
+++ c++/c002e.cpp
@@ -0,0 +1,90 @@
+/*
+ * Solution to /r/dailyprogrammer Challenge #1 Hard
+ * Jimmy Nguyen
+ *
+ * Prompt:
+ *     Create a program that will guess numbers between 1 and 100, and respond
+ * appropriately based on whether the users say that number is too high or too
+ * low
+ *
+ * Link to challenge:
+ *     https://www.reddit.com/r/dailyprogrammer/comments/pii6j/difficult_challenge_1/
+ */
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+void print_menu()
+{
+	std::cout << "\n==================================================\n";
+	std::cout << "\n\t1. F = M * A";
+	std::cout << "\n\t2. M = F / A";
+	std::cout << "\n\t3. A = F / M";
+	std::cout << "\n\tq. exit";
+	std::cout << "\n\n\t-------------------------\n";
+	std::cout << "\n\t>> ";
+}
+
+std::string input(std::string prompt)
+{
+	std::string input;
+	std::cout << prompt;
+	std::getline(std::cin, input);
+	return input;
+}
+
+void prompt_and_compute(std::string desired_variable)
+{
+	std::vector<std::string> variables = {"F", "M", "A"};
+	for (int i = 0; i < variables.size(); i++)
+	{
+		if (variables[i] == desired_variable)
+		{
+			variables.erase(variables.begin() + i);
+			break;
+		}
+	}
+	std::cout << std::endl;
+	printf("\t%s = ", variables[0].c_str());
+	double input1 = std::stod(input(""));
+	printf("\t%s = ", variables[1].c_str());
+	double input2 = std::stod(input(""));
+	double output;
+	if (desired_variable == "M" || desired_variable == "A")
+	{
+		output = input1 / input2;
+	}
+	else
+	{
+		output = input1 * input2;
+	}
+	printf("\n\t%s = %.2f", desired_variable.c_str(), output);
+	printf("\n\n\tPress ENTER to go back to the menu...");
+	input("");
+}
+
+bool select_formula(std::string command)
+{
+	bool is_terminate = false;
+	if (command == "1") {
+		prompt_and_compute("F");
+	} else if (command == "2") {
+		prompt_and_compute("M");
+	} else if (command == "3") {
+		prompt_and_compute("A");
+	} else if (command == "q") {
+		is_terminate = true;
+	}
+	return is_terminate;
+}
+
+int main(int argc, char *argv[])
+{
+	bool is_terminate = false;
+	while (!is_terminate)
+	{
+		print_menu();
+		is_terminate = select_formula(input(""));
+	}
+	return 0;
+}
