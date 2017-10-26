5c2fc6da2ab600f00ec1cf45833511b52f2d3f26
jimmynguyen <jimmynguyen@protonmail.com>
2017-10-26 10:25:28 -0400

    refactored code to match java equivalent

--- octave/c002i.m
+++ octave/c002i.m
@@ -13,101 +13,104 @@
 % Link to challenge:
 %     https://www.reddit.com/r/dailyprogrammer/comments/pjbuj/intermediate_challenge_2/
 function c002i(file_name)
+	validate_file(file_name);
 	paths = read_story(file_name);
-	fprintf('\n==================================================\n');
-	name = input(sprintf('\n> What is your name?\n> '), 's');
-	fprintf('\n> Welcome %s! Let the adventure begin...\n', name);
+	greet_user();
 	is_terminate = false;
 	while ~is_terminate
-		path = get_path(paths, '@ START');
+		path = get_path(paths, 'START');
 		while ~isempty(path.options) || ~isempty(path.next_path_id)
-			if ~isempty(path.description)
-				fprintf('\n%s', path.description);
-			end
+			print_description(path);
 			if ~isempty(path.options)
-				options = parse_options(path.options);
-				key = '';
-				while ~any(strcmpi(key, {options.key}))
-					for i = 1:length(path.options)
-						fprintf('\n%s', path.options{i});
-					end
-					key = input(sprintf('\n\n> '), 's');
-					if any(strcmpi(key, {options.key}))
-						path = get_path(paths, options(strcmpi(key, {options.key})).path_id);
-					else
-						fprintf('\n> Invalid option. Please select one of the following options:');
-					end
-				end
+				path = get_next_path(paths, path);
 			else
-				path.next_path_id(1) = '@';
 				path = get_path(paths, path.next_path_id);
 			end
 		end
-		fprintf('\n%s', path.description);
-		command = input(sprintf('\n> Do you want to start over? (y|n)\n> '), 's');
-		while ~any(strcmpi(command, {'y', 'n'}))
-			fprintf('\n> Invalid option. Please enter a valid option:');
-			command = input(sprintf('\n> Do you want to start over? (y|n)\n> '), 's');
-		end
-		if strcmpi(command, 'n')
-			is_terminate = true;
-		end
+		print_description(path);
+		is_terminate = prompt_termination();
 	end
 
-function paths = read_story(file_name)
+function validate_file(file_name)
 	if ~exist(file_name, 'file')
 		error('File does not exist');
 	end
+
+function file = read_file(file_name)
 	fh = fopen(file_name);
 	file = textscan(fh, '%s', 'Delimiter', '\n');
-	file = file{1};
-	ndx = find(cellfun(@(x) ~isempty(strfind(x, '@')), file, 'UniformOutput', true));
-	paths = {};
-	for i = 1:length(ndx)
-		start_ndx = ndx(i)+1;
-		if i == length(ndx)
-			end_ndx = length(file);
-		else
-			end_ndx = ndx(i+1)-1;
-		end
-		paths = [paths; [{file{ndx(i)}}, {file(start_ndx:end_ndx)}]];
-	end
 	fclose(fh);
+	file = file{1};
+
+function option = parse_option(line)
+	[key, option_line] = strtok(line, '- ');
+	[description, option_line] = strtok(regexprep(option_line, '^(-| )+', ''), '-');
+	path_id = strtok(option_line, '- ');
+	option = struct('key', key, 'description', description, 'path_id', path_id, 'line', line);
+
+function paths = read_story(file_name)
+	file = read_file(file_name);
+	paths = struct([]);
+	for i = 1:length(file)
+		line = strtrim(file{i});
+		if ~isempty(line)
+			switch line(1)
+				case '@'
+					paths(end+1).path_id = regexprep(line, '^(@| )+', '');
+					paths(end).description = '';
+				case '>'
+					paths(end).description = sprintf('%s%s\n', paths(end).description, line);
+				case '-'
+					paths(end).options(end+1) = parse_option(line);
+				case '='
+					paths(end).next_path_id = regexprep(line, '^(=| )+', '');
+			end
+		end
+	end
+
+function greet_user()
+	fprintf('\n==================================================\n');
+	name = input(sprintf('\n> What is your name?\n> '), 's');
+	fprintf('\n> Welcome %s! Let the adventure begin...\n', name);
+
+function print_description(path)
+	if ~isempty(path.description)
+		fprintf('\n%s', path.description);
+	end
+
+function print_options(path)
+	for i = 1:length(path.options)
+		fprintf('\n%s', path.options(i).line);
+	end
+
+function path = get_next_path(paths, path)
+	key = '';
+	options = path.options;
+	while ~any(strcmpi(key, {options.key}))
+		print_options(path);
+		key = input(sprintf('\n\n> '), 's');
+		if any(strcmpi(key, {options.key}))
+			path = get_path(paths, options(strcmpi(key, {options.key})).path_id);
+		else
+			fprintf('\n> Invalid option. Please select one of the following options:');
+		end
+	end
 
 function path = get_path(paths, path_id)
-	if path_id(1) ~= '@'
-		path_id = sprintf('@ %s', path_id);
+	if ~any(strcmpi(path_id, {paths.path_id}))
+		error('Error in .story file. Path "%s" not found', path_id);
 	end
-	path = [];
-	for i = 1:length(paths)
-		if strcmpi(paths{i, 1}, path_id)
-			path = parse_path_lines(paths{i, 2});
-			break;
-		end
-	end
-	if isempty(path)
-		error('Error in .story file. Path "%s" not found', path_id(3:end));
-	end
+	path = paths(strcmpi(path_id, {paths.path_id}));
 
-function path = parse_path_lines(path_lines)
-	path = struct('description', '', 'options', {{}}, 'next_path_id', '');
-	for i = 1:length(path_lines)
-		switch path_lines{i}(1)
-			case '>'
-				path.description = sprintf('%s%s\n', path.description, path_lines{i});
-			case '-'
-				path.options{end+1} = path_lines{i};
-			case '='
-				path.next_path_id = path_lines{i};
+function is_terminate = prompt_termination()
+	is_terminate = false;
+	command = -1;
+	while isnumeric(command) || ~any(strcmpi(command, {'y', 'n'}))
+		if ischar(command)
+			fprintf('\n> Invalid option. Please enter a valid option.\n');
 		end
+		command = input(sprintf('\n> Do you want to start over? (y|n)\n> '), 's');
 	end
-
-function options = parse_options(option_lines)
-	options = struct([]);
-	for i = 1:length(option_lines)
-		option_line = option_lines{i};
-		[key, option_line] = strtok(option_line, '- ');
-		[description, option_line] = strtok(option_line(3:end), '-');
-		[path_id, option_line] = strtok(option_line, '- ');
-		options(end+1) = struct('key', key, 'description', description, 'path_id', path_id);
+	if strcmpi(command, 'n')
+		is_terminate = true;
 	end
