5e843f0cd0de98f7b1f1c904d8cdb47e837bc3f9
jimmynguyen <jimmynguyen@protonmail.com>
2017-10-27 00:56:19 -0400

    initialized hard challenge 2 octave solution

---
+++ octave/c002h.m
@@ -0,0 +1,17 @@
+% Solution to /r/dailyprogrammer Challenge #2 Hard
+% Jimmy Nguyen
+%
+% Prompt:
+%     Create a stopwatch program. This program should have start, stop, and lap
+% options, and it should write out to a file to be viewed later.
+%
+% Link to challenge:
+%     https://www.reddit.com/r/dailyprogrammer/comments/pjsdx/difficult_challenge_2/
+function c002h()
+	fprintf('\n==================================================');
+	fprintf('\n> STOPWATCH');
+	fprintf('\n==================================================');
+	is_terminate = false;
+	while ~is_terminate
+
+	end
